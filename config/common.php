1d509f3a4645027e4d61be6854442f66d2118422
Alexander Solodov <notdest@gmail.com>
2020-07-20 06:47:44 +0300

    Task #125605 - Парсим инстаграм

---
+++ config/common.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+    'instagram' => (object) [
+        'login'     => env('INSTAGRAM_LOGIN',       'alexk6116'),
+        'password'  => env('INSTAGRAM_PASSWORD',    'sdfk.uy5wr,'),
+    ],
+
+];
---
+++ database/migrations/2020_07_20_000431_create_sources_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSourcesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sources', function (Blueprint $table) {
+            $table->increments('id');
+            $table->boolean('active');
+            $table->enum('type', ['instagram', 'facebook']);
+            $table->string('code', 255);
+            $table->string('name', 255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sources');
+    }
+}
