662845ceed20155f60d38826f546477484cc3873
Alexander Solodov <notdest@gmail.com>
2020-08-02 02:27:47 +0300

    Task #126415 - Прикрутил шаблон и две пустые страницы

---
+++ resources/views/platforms/alphabet.blade.php
@@ -0,0 +1,31 @@
+
+@extends('layouts.main')
+
+@section('content')
+
+    <h2>Section title Alphabet</h2>
+    <div class="table-responsive">
+        <table class="table table-striped table-sm">
+            <thead>
+            <tr>
+                <th>#</th>
+                <th>Header</th>
+                <th>Header</th>
+                <th>Header</th>
+                <th>Header</th>
+            </tr>
+            </thead>
+            <tbody>
+            @for ($i=0;$i<50;$i++)
+                <tr>
+                    <td>1,001</td>
+                    <td>Lorem</td>
+                    <td>ipsum</td>
+                    <td>dolor</td>
+                    <td>sit</td>
+                </tr>
+            @endfor
+            </tbody>
+        </table>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/platforms/list',       'PlatformController@list');
+Route::get('/platforms/alphabet',   'PlatformController@alphabet');
+
