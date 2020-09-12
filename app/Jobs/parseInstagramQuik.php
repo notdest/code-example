0e9516c244be34a2f9b8f4c5baa6af50fb478f5d
Alexander Solodov <notdest@gmail.com>
2020-09-12 16:37:03 +0300

    Task #131565 - Парсинг инстаграма в один запрос

---
+++ app/Jobs/parseInstagramQuik.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
+use InstagramScraper\Instagram;
+use Phpfastcache\Helper\Psr16Adapter;
+
+class parseInstagramQuik implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $config     = app('config')['common.instagram'];
+        $instagram  = Instagram::withCredentials($config->login, $config->password, new Psr16Adapter('Files'));
+        $instagram->login(); // по умолчанию ищет закешированную saveSession()
+        $instagram->saveSession();
+
+        $posts  = $instagram->getFeed();
+
+        $usernames  = $controlUsernames = $ids = [];
+        foreach ($posts as $k => $post){
+            $id     = $post->getId();
+            $ids[]  =$id;
+
+            $owner  = $post->getOwner();        // иногда не срабатывает, приходится контролировать
+            if(!$owner){
+                sleep(3);
+                $owner = $post->getOwner();
+            }
+
+            if($owner){                         // из-за несрабатываний контролируем, был ли юзернэйм
+                $username    = $owner->getUsername();
+                $usernames[] =  $username;
+                $controlUsernames[$id] = $username;
+            }else{
+                $controlUsernames[$id] = false;
+            }
+
+        }
+        $localPosts = DB::table('posts')
+                        ->whereIn('numericalId', $ids)
+                        ->pluck('numericalId')->all();
+
+        $sourceIds  = DB::table('sources')
+                        ->where('type', 'instagram')
+                        ->whereIn('code', array_unique($usernames))
+                        ->pluck( 'id','code')->all();
+
+
+        $insertion  = [];
+        foreach ($posts as $post){
+            if(in_array($post->getId(),$localPosts)){                        // если уже сохраняли то пропускаем
+                break;
+            }
+
+            $newRow = [
+                'postId'        => $post->getShortCode(),
+                'numericalId'   => $post->getId(),
+                'createdTime'   => date("Y-m-d H:i:s",$post->getCreatedTime()+(3*3600)),
+                'text'          => $post->getCaption(),
+                'image'         => ($post->getImageStandardResolutionUrl())  ?: $post->getImageHighResolutionUrl(),
+            ];
+
+            if( $controlUsernames[$post->getId()] ){           // У этого поста получен ник автора
+                $username = $post->getOwner()->getUsername();
+
+                if(isset($sourceIds[$username])) {
+                    $newRow['sourceId'] = $sourceIds[$username];
+                }else{
+                    $source = new \App\Source();
+                    $source->personId   = 0;
+                    $source->active     = 1;
+                    $source->type       = 'instagram';
+                    $source->code       = $username;
+
+                    $source->save();
+
+                    $newRow['sourceId'] = $source->id;
+                }
+
+                $insertion[] = $newRow;
+            }
+
+        }
+
+        DB::table('posts')->insert( $insertion );
+    }
+}
