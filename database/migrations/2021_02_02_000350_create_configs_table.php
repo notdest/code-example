d740b41cf8e8012b92639487bd089e5abf5f43ac
Alexander Solodov <notdest@gmail.com>
2021-02-09 12:16:23 +0300

    Task #157415 - Уменьшил длинну индекса в configs, чтобы не вылетало

--- database/migrations/2021_02_02_000350_create_configs_table.php
+++ database/migrations/2021_02_02_000350_create_configs_table.php
@@ -17,7 +17,7 @@ class CreateConfigsTable extends Migration
             $table->id();
             $table->timestamps();
 
-            $table->string('name')->unique();
+            $table->string('name',190)->unique();
             $table->text('settings');
         });
     }
